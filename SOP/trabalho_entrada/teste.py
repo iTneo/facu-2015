ab9a8fac25100ff04938e044fb4ec77582586c6a
Jonathan Juares Beber <jonathanbeber@gmail.com>
2015-08-11 19:35:50 -0300

    Trabalho de entrada para SOP

---
+++ SOP/trabalho_entrada/teste.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import sys
+import time
+print("aishdoiahdiashdaosdhasdhaosihd")
+time.sleep(1)
+sys.stderr.write("EUSOUUMERRO")
+sys.stdout.write("ihadaçsjdasjdaksdlalskdçlaskd")
---
+++ SOP/trabalho_entrada/traba.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+# Variáveis estáticas
+OPCOES = ['receita', 'despesa', 'saldo']
+
+# Inicializar variáveis
+saldo=0
+receita=0
+despesas=0
+
+
+while True:
+    try:
+	linha = raw_input()
+    except EOFError:
+        break
+    linha = linha.split(":")
+    tipo_de_dados = linha[0]
+    if tipo_de_dados in OPCOES: 
+	valor = linha[1]
+        if tipo_de_dados in ['saldo', 'receita']:
+	    if tipo_de_dados == 'saldo':
+		saldo += float(valor.split(" ")[1])
+	    else: 
+	        valor = float(valor.split(" ")[2])
+		saldo += valor
+		receita += valor
+	else:
+	    valor = float(valor.split(" ")[2])
+	    despesas += valor
+	    saldo -= valor
+    else:
+	sys.stderr.write("Erro: Opção não válida!\n")
+
+# Imprime resultados
+print str(saldo) + " reais de saldo"
+print str(despesas) + " reais de despesas"
+print str(receita) + " reais de receita"
