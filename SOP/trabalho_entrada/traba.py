7101c079c9468b32d70962bd3f8a6a09814ff9aa
Jonathan Juares Beber <jonathanbeber@gmail.com>
2015-08-11 20:43:38 -0300

    corrigidos tabs

--- SOP/trabalho_entrada/traba.py
+++ SOP/trabalho_entrada/traba.py
@@ -12,31 +12,30 @@ saldo=0
 receita=0
 despesas=0
 
-
 while True:
     try:
-	linha = raw_input()
+        linha = raw_input()
     except EOFError:
         break
     linha = linha.split(":")
     tipo_de_dados = linha[0]
-    if tipo_de_dados in OPCOES: 
-	valor = linha[1]
+    if tipo_de_dados in OPCOES:
+        valor = linha[1]
         if tipo_de_dados in ['saldo', 'receita']:
-	    if tipo_de_dados == 'saldo':
-		saldo += float(valor.split(" ")[1])
-	    else: 
+	        if tipo_de_dados == 'saldo':
+		        saldo += float(valor.split(" ")[1])
+	        else:
+	            valor = float(valor.split(" ")[2])
+		        saldo += valor
+		        receita += valor
+	    else:
 	        valor = float(valor.split(" ")[2])
-		saldo += valor
-		receita += valor
-	else:
-	    valor = float(valor.split(" ")[2])
-	    despesas += valor
-	    saldo -= valor
+	        despesas += valor
+	        saldo -= valor
     else:
-		hora = datetime.now()
-		sys.stderr.write(hora.strftime("%H:%M - %d:%m:%Y - "))
-		sys.stderr.write("Erro: Opção não válida!\n")
+        hora = datetime.now()
+        sys.stderr.write(hora.strftime("%H:%M - %d:%m:%Y - "))
+        sys.stderr.write("Erro: Opção não válida!\n")
 
 # Imprime resultados
 print str(saldo) + " reais de saldo"
