e5c80758cd7d3a9242c2b9716235336aabc9b779
Jonathan Juares Beber <jonathanbeber@gmail.com>
2015-08-21 21:56:59 -0300

    Ajustada agenda

--- POO/agenda-mysql/Agenda.java
+++ POO/agenda-mysql/Agenda.java
@@ -32,16 +32,39 @@ public class Agenda {
 			System.out.println("Falha ao adicionar !");
 		}
 	}
-	public void consultar(){
+
+	public String consultar_id(){
 		System.out.println("Digite um nome");
 		String nomeBusca = entrada.next();
 		ResultSet rs = null;
 		try {
-			PreparedStatement pst = (PreparedStatement) bo.conn.prepareStatement("select nome, endereco, telefone, email from agenda where nome like ?");
+			PreparedStatement pst = (PreparedStatement) bo.conn.prepareStatement("select id, nome from agenda where nome like ?");
 			nomeBusca = "%" + nomeBusca + "%";
 			pst.setString(1, nomeBusca);
 			rs = pst.executeQuery();
-			System.out.println("Nome Encontrado!");
+		} catch (SQLException e) {
+			System.out.println("Nome não Encontrado!\n");
+			return "-1";
+		}
+		try{
+			System.out.println("ID - Nome");
+			while(rs.next()) {
+				System.out.println(rs.getInt("id") + " - " + rs.getString("nome"));
+			}
+		} catch (SQLException e) {
+			System.out.println("Falha ao ler dados");
+		}
+		System.out.println("Digite o id desejado:");
+		return entrada.next();
+	}
+	
+	public void consultar(){
+		String id = consultar_id();
+		ResultSet rs = null;
+		try {
+			PreparedStatement pst = (PreparedStatement) bo.conn.prepareStatement("select nome, endereco, telefone, email from agenda where id like ?");
+			pst.setString(1, id);
+			rs = pst.executeQuery();
 		} catch (SQLException e) {
 			System.out.println("Nome não Encontrado!\n");
 		}
@@ -54,19 +77,18 @@ public class Agenda {
 				System.out.println("Nome: " + rs.getString("nome"));
 				System.out.println("Endereco: " + rs.getString("endereco"));
 				System.out.println("E-mail: " + rs.getString("email"));
-				System.out.println("######################################");
+				System.out.println("#######################################");
 			}
 		} catch (SQLException e) {
 			System.out.println("Falha ao ler dados");
 		}
 	}
 	public void remover() {
-		System.out.println("Digite um nome");
-		String nomeDeleta = entrada.next();
+		String id = consultar_id();
 		try {
 			PreparedStatement pst = (PreparedStatement) bo.conn
-					.prepareStatement("delete from agenda where nome = ?");
-			pst.setString(1, nomeDeleta);
+					.prepareStatement("delete from agenda where id = ?");
+			pst.setString(1, id);
 			pst.execute();
 			System.out.println("Removido com sucesso!");
 		} catch (SQLException ex) {
@@ -82,44 +104,45 @@ public class Agenda {
 		Boolean emailChange = false;
 		
 		String sql = "update agenda set ";
-		System.out.println("Digite um nome para alterar esse contato");
-		String nomeAltera = entrada.next();
+		String id = consultar_id();
+		System.out.println("Digite os novos valores ou 'n' para não alterar");
 		System.out.println("Digite o novo nome:");
 		entrada.nextLine();
 		String nomeAltera2 = entrada.nextLine();
-		if (nomeAltera2 != "") {
+		if (nomeAltera2 != "n") {
 			sql += "nome = ?,";  
 			nomeChange = true;
 			alteracoes = true;
 		}
 		System.out.println("Digite o novo endereço:");
 		String enderecoAltera = entrada.next();
-		if (enderecoAltera != "") {
+		if (enderecoAltera != "n") {
 			sql += "endereco = ?,";
 			enderecoChange = true;
 			alteracoes = true;
 		}
 		System.out.println("Digite o novo número:");
 		String numeroAltera = entrada.next();
-		if (numeroAltera != "") {
+		System.out.println(numeroAltera == "n");
+		if (numeroAltera != "n") {
 			sql += "telefone= ?,"; 
 			numeroChange = true;
 			alteracoes = true;
 		}
 		System.out.println("Digite o novo e-mail:");
 		String emailAltera = entrada.next();
-		if (emailAltera != "") {
+		if (emailAltera != "n") {
 			sql += "email = ? "; 
 			emailChange = true;
 			alteracoes = true;
 		} else {
 			sql = sql.substring(0, sql.length());
 		}	
+		System.out.println(sql);
 		
-		sql += "where nome = ?";
+		sql += "where id = ?";
 		try {
 			if(alteracoes){
-				System.out.println(sql);
 				PreparedStatement pst = (PreparedStatement) bo.conn
 						.prepareStatement(sql);
 				int pos = 1;
@@ -139,7 +162,7 @@ public class Agenda {
 					pst.setString(pos, emailAltera);
 					pos++;
 				}
-				pst.setString(pos, nomeAltera);
+				pst.setString(pos, id);
 				pst.execute();
 				System.out.println("Alterado com sucesso!");
 			}else{
